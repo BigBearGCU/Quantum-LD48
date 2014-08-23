2407642c6718c960be3d5978f0dfd073b16c71c2
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2014-08-23 15:35:13 +0100

    First Prototype

---
+++ Assets/Scripts/LeptonParticle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeptonParticle : MonoBehaviour,FundamentalParticle{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnEnable()
+    {
+        Enable();
+    }
+
+    public void OnDisable()
+    {
+        Disable();
+    }
+
+    public void Enable()
+    {
+        collider.enabled = true;
+    }
+
+    public void Disable()
+    {
+        collider.enabled = false;
+    }
+}
---
+++ Assets/Scripts/QuantumBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(LeptonParticle))]
+public class QuantumBehaviour : MonoBehaviour
+{
+    public LeptonParticle lepton;
+    // Use this for initialization
+    void Start()
+    {
+        lepton = GetComponent<LeptonParticle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
