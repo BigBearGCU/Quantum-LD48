62990abe64ff89d6f14e7d0b054b8ee276e575b3
Brian McDonald <brian.mcdonald1980@googlemail.com>
2014-08-24 11:42:46 +0100

    Finished quarks

--- Assets/Scripts/Particles/QuantumBehaviour.cs
+++ Assets/Scripts/Particles/QuantumBehaviour.cs
@@ -5,26 +5,29 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(LeptonParticle))]
+[RequireComponent(typeof(QuarkParticle))]
 public class QuantumBehaviour : MonoBehaviour
 {
     public LeptonParticle lepton;
-    public List<QuarkParticle> quarks = new List<QuarkParticle>();
+    public QuarkParticle quark;
     // Use this for initialization
     void Start()
     {
         lepton = GetComponent<LeptonParticle>();
-        QuarkParticle p = new QuarkParticle();
-        p.owner = gameObject;
-        quarks.Add(p);
+        quark= GetComponent<QuarkParticle>();
+
+        //lepton.Disable();
+        quark.Disable();
+
+        lepton.enabled = true;
+        quark.enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         lepton.Apply();
-        foreach(QuarkParticle q in quarks)
-        {
-            q.Apply();
-        }
+        quark.Apply();
     }
 }
---
+++ Assets/Scripts/Particles/QuarkParticle.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuarkParticle : MonoBehaviour,FundamentalParticle
+{
+
+    public float range = 10.0f;
+    RaycastHit hitInfo = new RaycastHit();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnEnable()
+    {
+        Enable();
+    }
+
+    public void OnDisable()
+    {
+        Disable();
+    }
+
+    public void Enable()
+    {
+        
+    }
+
+    public void Disable()
+    {
+        GameObject[] attractables = GameObject.FindGameObjectsWithTag("Attractable");
+        foreach(GameObject o in attractables)
+        {
+            o.SendMessage("Captured", false);
+        }
+    }
+
+
+    public void Apply()
+    {
+        if (enabled)
+        {
+            //do a spehere cast, find objects that attriable and pull towards(think tractor beam)
+            if (Physics.SphereCast(transform.position, range, transform.forward, out hitInfo))
+            {
+                //lets grab collider collider 
+                Collider c = hitInfo.collider;
+                if (c.tag == "Attractable")
+                {
+                    //send message to say that has been captured
+                    c.gameObject.SendMessage("Captured", true);
+                    Vector3 direction = (transform.position - c.transform.position);
+                    float magnitude = direction.magnitude;
+                    direction.Normalize();
+                    c.gameObject.rigidbody.AddForce(direction * magnitude);
+
+                }
+            }
+        }
+    }
+}
