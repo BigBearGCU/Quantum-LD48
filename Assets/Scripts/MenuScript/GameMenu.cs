59415a135a4209f3f7e7c0040b35c9855c3f72d5
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2014-08-28 00:57:24 +0100

    Added some changes to the menu and the UI.

--- Assets/Scripts/MenuScript/GameMenu.cs
+++ Assets/Scripts/MenuScript/GameMenu.cs
@@ -1,22 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
+using System;
 public class GameMenu : MonoBehaviour {
 
     public GameObject player;
+    public UIAtlas keyboard;
+    public UIAtlas xbox360Joypad;
+    public UIAtlas PS3Joypad;
+
+    public List<GameObject> uiHints;
+
     private QuantumBehaviour quantumPowers;
+    
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         //set button state
         quantumPowers = player.GetComponent<QuantumBehaviour>();
-
+        //Get user Prefs for joypad
+        ChangeUIHints();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+
 	}
+
+    void ChangeUIHints()
+    {
+        bool joyPad = Convert.ToBoolean(PlayerPrefs.GetInt("usejoypad"));
+        if (joyPad)
+        {
+            UISprite s = null;
+            foreach(GameObject go in uiHints)
+            {
+                s = go.GetComponent<UISprite>();
+                s.atlas = xbox360Joypad;
+            }
+        }
+    }
 
     public void OnNucleonButtonDown()
     {
--- Assets/Scripts/MenuScript/MainMenuScript.cs
+++ Assets/Scripts/MenuScript/MainMenuScript.cs
@@ -1,21 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MainMenuScript : MonoBehaviour {
 
     GameObject levelManager;
+    List<GameObject> Buttons=new List<GameObject>();
+    bool joypadControlsEnabled = false;
+    GameObject prompt;
+    Vector2 lastMousePos=Vector2.zero;
 	// Use this for initialization
 	void Start () {
+        //Grab and cache all buttons
+        Transform buttonPanel=transform.FindChild("ButtonPanel");
+        prompt = transform.Find("Prompt").gameObject;
+        prompt.SetActive(false);
+        for (int i = 0; i < buttonPanel.childCount; ++i)
+        {
+            Buttons.Add(buttonPanel.GetChild(i).gameObject);
+        }
         levelManager = GameObject.Find("LevelManager");
 #if UNITY_WEBPLAYER
-        GameObject exitButton=GameObject.Find("ExitButton");
-        exitButton.collider.enabled=false;
+        DisableExitButton();
 #endif
                   }
-	
+
+    void ToggleJoypadControls(bool toggle)
+    {
+        if (toggle && !joypadControlsEnabled)
+        {
+            foreach (GameObject go in Buttons)
+            {
+                UIKeyNavigation nav = go.GetComponent<UIKeyNavigation>();
+                nav.enabled = true;
+            }
+            joypadControlsEnabled = true;
+            prompt.SetActive(toggle);
+            PlayerPrefs.SetInt("usejoypad", 1);
+#if UNITY_WEBPLAYER
+            DisableExitButton();
+#endif
+        }
+        else if (!toggle && joypadControlsEnabled)
+        {
+            foreach (GameObject go in Buttons)
+            {
+                UIKeyNavigation nav = go.GetComponent<UIKeyNavigation>();
+                UIButton button=go.GetComponent<UIButton>();
+                button.state=UIButton.State.Normal;
+                nav.enabled = false;
+            }
+            prompt.SetActive(toggle);
+            joypadControlsEnabled = false;
+            PlayerPrefs.SetInt("usejoypad", 0);
+#if UNITY_WEBPLAYER
+            DisableExitButton();
+#endif
+        }
+    }
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        float axis = Input.GetAxis("JoypadVertical");
+        Vector2 currentMousePos=Input.mousePosition;
+        if (axis != 0.0f)
+        {
+            ToggleJoypadControls(true);
+        }
+
+        if (Vector2.Distance(currentMousePos,lastMousePos)>0.5f)
+        {
+            ToggleJoypadControls(false);
+        }
+        lastMousePos = currentMousePos;
 	}
 
     public void OnStartPressed()
@@ -31,5 +88,13 @@ public class MainMenuScript : MonoBehaviour {
     public void OnExitPressed()
     {
         Application.Quit();
+    }
+
+    public void DisableExitButton()
+    {
+        GameObject exitButton = GameObject.Find("ExitButton");
+        UIButton button = exitButton.GetComponent<UIButton>();
+        button.state = UIButton.State.Disabled;
+        exitButton.collider.enabled = false;
     }
 }
